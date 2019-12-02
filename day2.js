f3ffd2a60087e8fb3f87f1c897e3a958061bff58
Zdravko Mavkov <mavkov@hotmail.com>
2019-12-02 10:06:16 +0100

    finished day2 part 2

--- day2.js
+++ day2.js
@@ -1,38 +1,56 @@
 const fs = require('fs')
 let contents = fs.readFileSync('day2.txt', 'utf8');
 let inputData = contents.split(',').map(num => Number(num));
-let fakeData = [1,9,10,3,2,3,11,0,99,30,40,50];
-let workingData = [...inputData];
+let fakeData = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
 
-// console.log(workingData);
-workingData[1] = 12;
-workingData[2] = 2;
 
-for(let i = 0; i < workingData.length; i++){
-    // console.log('i: ', i);
+function intcode(noun, verb) {
+    let workingData = [...inputData];
+    // console.log(workingData);
+    workingData[1] = noun;
+    workingData[2] = verb;
 
-    let index0 = workingData[i];
-    let index1 = workingData[i + 1];
-    let index2 = workingData[i + 2];
-    let index3 = workingData[i + 3];
-    // console.log('index0: ', index0);
-    // console.log('index1: ', index1);
-    // console.log('index2: ', index2);
-    // console.log('index3: ', index3);
+    for (let i = 0; i < workingData.length; i++) {
+        // console.log('i: ', i);
 
-    if(index0 === 1){
-        workingData[index3] = workingData[index1] + workingData[index2];
+        let index0 = workingData[i];
+        let index1 = workingData[i + 1];
+        let index2 = workingData[i + 2];
+        let index3 = workingData[i + 3];
+        // console.log('index0: ', index0);
+        // console.log('index1: ', index1);
+        // console.log('index2: ', index2);
+        // console.log('index3: ', index3);
+
+        if (index0 === 1) {
+            workingData[index3] = workingData[index1] + workingData[index2];
+        }
+        if (index0 === 2) {
+            workingData[index3] = workingData[index1] * workingData[index2];
+        }
+        if (index0 === 99) {
+            break;
+        }
+        i = i + 3;
+        // console.log('------------------');
+        // console.log('Data: ', workingData);
+        // console.log('------------------');
     }
-    if(index0 === 2){
-        workingData[index3] = workingData[index1] * workingData[index2];
-    }
-    if(index0 === 99){
-        break;
-    }
-    i = i + 3;
-    // console.log('------------------');
-    // console.log('Data: ', workingData);
-    // console.log('------------------');
+
+    // console.log(workingData[0]);
+    return workingData[0];
 }
 
-console.log(workingData[0]);
+for(let i = 0; i <= 99; i++){
+    for(let j = 0; j <= 99; j++){
+        // console.log('noun: ', i ,'verb: ', j);
+        // console.log(intcode(i, j));
+        if(intcode(i, j) === 19690720){
+            console.log('noun: ', i ,'verb: ', j);
+            console.log('answer: ', 100 * i + j);
+            break;
+        }
+    }
+}
+
+
